f1771d49e7d13794429bdb56300cab8b3705104a
shafayet <shafayetsec09@gmail.com>
2025-04-09 00:17:34 +0600

    tailwind css install

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
+     <div className="bg-green-500 text-white p-6 rounded-xl text-center shadow-lg">
+    <h1 className="text-3xl font-bold">Tailwind is working! 🚀</h1>
+    <p className="mt-2 text-lg">If you see this with green background and white text, you're good to go!</p>
+  </div>
     </div>
   );
 }
