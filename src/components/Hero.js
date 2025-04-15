c3335bdb5f6e03a1448836ffe87532292f230903
shafayet <shafayetsec09@gmail.com>
2025-04-15 12:21:35 +0600

    hero section update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,14 @@
+import style from '../assets/styles/components/Hero.module.css'
+
+
 // src/components/Hero.jsx
 const Hero = () => (
-    <section id="hero" className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-center p-4">
-      <h1 className="text-5xl font-bold mb-4">Your Name</h1>
-      <p className="text-xl">Full Stack Developer | React Enthusiast</p>
+    <section id="hero" className="h-screen flex items-center justify-center p-4">
+      <div className="container flex flex-col justify-start">
+        <h2 className={`${style.heroSubHeader}`}>Hi, I'm</h2>
+        <h1 className={`${style.heroHeader}`}>Faria Islam</h1>
+        <p className={`${style.heroText}`}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+      </div>
     </section>
   );
   
