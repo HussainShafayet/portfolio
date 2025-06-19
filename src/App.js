caec1c126d135e84cfb01c71ad8bff68a1cf76d1
shafayet <shafayetsec09@gmail.com>
2025-06-19 11:53:46 +0600

    auto scroll when tab change

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ import {
   ScrollToTop,
   Skills,
 } from "./components";
+import ScrollToHash from "./components/ScrollToHash";
 
 const App = () => {
   return (
     <div className="min-h-screen md:container md:mx-auto px-4 bg-gradient-to-br from-gray-100 to-white dark:from-gray-900 dark:to-gray-800 text-gray-900 dark:text-gray-100">
+      <ScrollToHash />
       <Navbar />
 
       <main>
