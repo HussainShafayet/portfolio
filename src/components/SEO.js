df768d71886ae867db73543ef7cf3427d58731bc
shafayet <shafayetsec09@gmail.com>
2025-06-19 14:11:05 +0600

    og meta tag udpate

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -12,26 +12,21 @@ const SEO = () => (
     <meta name="keywords" content="JavaScript, React, Frontend, Web Developer, Portfolio, Next.js, Ember.js" />
     <meta name="author" content="Shafayet Hussain" />
     <meta name="robots" content="index, follow" />
+    <title>Shafayet Hussain | JavaScript Developer</title>
+    <meta name="description" content="Portfolio of Shafayet Hussain, a passionate JavaScript developer specializing in React, Next.js, and Ember.js." />
 
-    {/* Open Graph / Facebook */}
-    <meta property="og:type" content="website" />
     <meta property="og:url" content="https://shafayethussain.netlify.app/" />
+    <meta property="og:type" content="website" />
     <meta property="og:title" content="Shafayet Hussain | JavaScript Developer" />
-    <meta
-      property="og:description"
-      content="Portfolio of Shafayet Hussain, a passionate JavaScript developer specializing in React, Next.js, and Ember.js."
-    />
-    <meta property="og:image" content="https://shafayethussain.netlify.app/static/media/self.6866363a49d2d33be2ce.jpg" />
+    <meta property="og:description" content="Portfolio of Shafayet Hussain, a passionate JavaScript developer specializing in React, Next.js, and Ember.js." />
+    <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/3fbe6384-c4f6-4b56-aadd-576092ae39ef.jpg?token=pNAY4Ng2HdrHvXAoiBQIwkaNO3gC3O7mrv5m-_NAHbQ&height=900&width=1200&expires=33286320386" />
 
-    {/* Twitter */}
-    <meta property="twitter:card" content="summary_large_image" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:domain" content="shafayethussain.netlify.app" />
     <meta property="twitter:url" content="https://shafayethussain.netlify.app/" />
-    <meta property="twitter:title" content="Shafayet Hussain | JavaScript Developer" />
-    <meta
-      property="twitter:description"
-      content="Portfolio of Shafayet Hussain, a passionate JavaScript developer specializing in React, Next.js, and Ember.js."
-    />
-    <meta property="twitter:image" content="https://shafayethussain.netlify.app/static/media/self.6866363a49d2d33be2ce.jpg" />
+    <meta name="twitter:title" content="Shafayet Hussain | JavaScript Developer" />
+    <meta name="twitter:description" content="Portfolio of Shafayet Hussain, a passionate JavaScript developer specializing in React, Next.js, and Ember.js." />
+    <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/3fbe6384-c4f6-4b56-aadd-576092ae39ef.jpg?token=pNAY4Ng2HdrHvXAoiBQIwkaNO3gC3O7mrv5m-_NAHbQ&height=900&width=1200&expires=33286320386" />
 
     <link rel="canonical" href="https://shafayethussain.netlify.app/" />
   </Helmet>
